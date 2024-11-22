6eaa13e53943e54318ea88b30929495b2bd4aec8
thanhhai147 <thanhhai252004@gmail.com>
2024-11-22 14:44:58 +0700

    get-session, delete-session, delete-chat

--- backend/QALLM/urls/session.py
+++ backend/QALLM/urls/session.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from ..views.session import CreateSessionAPIView
+from ..views.session import CreateSessionAPIView, GetSessionByUserIdAPIView, DeleteSessionAPIView
 
 
 urlpatterns = [
     path('create-session', CreateSessionAPIView.as_view(), name='create-session'),
+    path('get-session', GetSessionByUserIdAPIView.as_view(), name='get-session'),
+    path('delete-session', DeleteSessionAPIView.as_view(), name='delete-session')
 ]
--- backend/QALLM/views/session.py
+++ backend/QALLM/views/session.py
@@ -3,6 +3,7 @@ from rest_framework.generics import GenericAPIView
 from rest_framework import status
 from ..models.user import User
 from ..models.session import Session
+from ..models.chat import Chat
 
 class CreateSessionAPIView(GenericAPIView):
     def post(self, request):
@@ -13,7 +14,7 @@ class CreateSessionAPIView(GenericAPIView):
             return Response(
                 {
                     "success": False,
-                    "message": "Thông tin người dùng không hợp lệ"
+                    "message": "Thông tin phiên không hợp lệ"
                 }, 
                 status=status.HTTP_400_BAD_REQUEST
             )
@@ -65,6 +66,121 @@ class CreateSessionAPIView(GenericAPIView):
                     "user_id": user_id,
                     "session_id": session_instance.session_id
                 }
+            }, 
+            status=status.HTTP_200_OK
+        )
+
+class GetSessionByUserIdAPIView(GenericAPIView):
+    def get(self, request):
+        params = request.query_params
+        try:
+            user_id = params['user_id']
+        except:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Thông tin phiên không hợp lệ"
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if not user_id:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Mã người dùng không hợp lệ"
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        try:
+            user_instance = User.objects.get(user_id=user_id)
+        except User.DoesNotExist:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Người dùng không tồn tại"
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        except:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Lỗi Database"
+                }, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        
+        try:
+            session_instances = Session.objects.filter(user_id=user_id)
+            session_ids = [session.session_id for session in session_instances]
+        except:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Lỗi Database"
+                }, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        
+        return Response(
+            {
+                "success": True,
+                "message": "Lấy thành công phiên",
+                "data": {
+                    "user_id": user_id,
+                    "session_ids": session_ids
+                }
+            }, 
+            status=status.HTTP_200_OK
+        )
+      
+class DeleteSessionAPIView(GenericAPIView):
+    def post(self, request):
+        data = request.data
+        try:
+            session_id = data['session_id']
+        except:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Thông tin phiên không hợp lệ"
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if not session_id:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Mã phiên không hợp lệ"
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        try:
+            session_instance = Session.objects.get(session_id=session_id)
+            session_instance.delete()
+        except Session.DoesNotExist:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Phiên không tồn tại"
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        except:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Lỗi Database"
+                }, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        
+        return Response(
+            {
+                "success": True,
+                "message": "Xóa thành công phiên"
             }, 
             status=status.HTTP_200_OK
         )
